f28b031c43ec7add1f241faf8e50aee307b13381
jackie <b02901122@ntu.edu.tw>
2016-11-18 13:05:56 +0800

    correct mistake

--- hw3/train_512_128.py
+++ hw3/train_512_128.py
@@ -30,9 +30,9 @@ u_data = np.concatenate((u_data,t_data),axis=0)
 ####################################################
 mc_path = model_path
 mc = ModelCheckpoint(mc_path, monitor='val_acc', verbose=0, save_best_only=True, mode='max')
-es = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
+es = EarlyStopping(monitor='val_loss', min_delta=0, patience=8, verbose=0, mode='auto')
 
-batch_size = 128
+batch_size = 32
 nb_classes = 10
 
 Train,T_label,Valid,V_label = dp.split_valid(l_data,label,ratio=0.1,is_seed=False,seed=6)
@@ -94,10 +94,10 @@ count = 0
 while(1):
     nb_epoch = 20
     if count == 0:
-        nb_epoch = 15
+        nb_epoch = 18
     train_datagen.fit(Train)
     t_history = model.fit_generator(train_datagen.flow(Train,T_label,batch_size=batch_size),
-                        samples_per_epoch=45000,nb_epoch=nb_epoch,callbacks=[mc,es],
+                        samples_per_epoch=4500*5,nb_epoch=nb_epoch,callbacks=[mc,es],
                         validation_data=(Valid,V_label))
     if count==6:
         break
@@ -106,7 +106,7 @@ while(1):
     print('\n%d data are predicted,shape = %r' %(predict.shape[0],predict.shape))
     max_eles = np.max(predict,axis=1)#(45000,)
     # if c >3:
-    index = np.argwhere(max_eles>0.85).flatten() #(?,)
+    index = np.argwhere(max_eles>0.99).flatten() #(?,)
     # else:
         # index = np.argwhere(max_eles>0.99).flatten() #(?,)
     compare = max_eles.reshape((max_eles.shape[0],1))[index] #(?,1)
@@ -121,7 +121,6 @@ while(1):
                         samples_per_epoch=new_data.shape[0],nb_epoch=nb_epoch,callbacks=[mc,es],
                         validation_data=(Valid,V_label))
     count+=1
-    break
 
 
 if keras.backend.tensorflow_backend._SESSION:
